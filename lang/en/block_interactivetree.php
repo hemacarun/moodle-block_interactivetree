6ae37d522cf06f3c736b33373b5a217060dd10ab
hemacarun <hema.gowda88@gmail.com>
2016-12-07 17:44:20 +0530

    Update block_interactivetree.php

--- lang/en/block_interactivetree.php
+++ lang/en/block_interactivetree.php
@@ -18,7 +18,7 @@ $string['interactivetree:addinstance'] = 'Add a jquery interactive tree block';
 $string['addurl'] = 'Add url to node';
 $string['selectnode'] = 'Select node';
 $string['interactivetree:manage'] = 'Allow user to create,rename and delete the node of a interactivetree block';
-$string['interactivetree:myaddinstance'] = 'Add a new interactive tree block to Dashboard';
+$string['interactivetree:myaddinstance'] = 'Add a new interactive tree block to Dashboar';
 
 
 
